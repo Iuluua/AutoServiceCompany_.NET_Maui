f754515a504bab413883073251286eed715e8bf7
Iulia Anca <iulia.anca@evozon.com>
2025-01-06 15:04:28 +0200

    Add map for clients

--- ClientPage.xaml.cs
+++ ClientPage.xaml.cs
@@ -30,4 +30,21 @@ public partial class ClientPage : ContentPage
             await Navigation.PopAsync();
         }
     }
+
+
+    async void OnShowMapButtonClicked(object sender, EventArgs e)
+    {
+        //var client = (Client)BindingContext;
+        //var address = client.Adresa;
+        //var locations = await Geocoding.GetLocationsAsync(address);
+
+        var options = new MapLaunchOptions
+        {
+            Name = "Adresa clientului"
+        };
+        //var clientlocation = locations?.FirstOrDefault();
+        var clientlocation = new Location(46.77562320574969, 23.621155970165358);//pentru Windows Machine
+
+        await Map.OpenAsync(clientlocation, options);
+    }
 }
